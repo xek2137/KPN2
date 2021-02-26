3b1e61cb46bd93728409683bda3cc8e7021c7bbe
Adrian Piech <adrianpiech.git@gmail.com>
2021-02-26 01:52:14 +0100

    Add computer vs player script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if (randomNumber == 1) {
+    computerMove = 'kamien';
+}
+else if (randomNumber == 2) {
+    computerMove = 'papier';
+}
+else if (randomNumber == 3) {
+    computerMove = 'nozyce';
+}
+
+printMessage('Moj ruch to: ' + computerMove);
+
 let playerInput = prompt('Wybierz swój ruch! 1: kamien, 2: papier, 3: nozyce.');
 
 console.log('Gracz wpisal: ' + playerInput);
