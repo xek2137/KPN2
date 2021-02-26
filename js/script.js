826f39193ef1091ef41acc50f67fd6f448e1ff11
Adrian Piech <adrianpiech.git@gmail.com>
2021-02-26 01:27:51 +0100

    Add computerMove script

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
-let computerMove = 'kamien';
-let playerMove = 'papier';
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagralem ' + computerMove + '! Jesli Twoj ruch to ' + playerMove + ', to wygrywasz!');
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if (randomNumber == 1) {
+    computerMove = 'kamien';
+}
+else if (randomNumber == 2) {
+    computerMove = 'papier';
+}
+else if (randomNumber == 3) {
+    computerMove = 'nozyce';
+}
+
+    printMessage('Moj ruch to: ' + computerMove);
